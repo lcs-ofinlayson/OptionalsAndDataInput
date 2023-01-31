935ed6850ab6a663efbc3c55e8d092209b9dc5d9
Russell Gordon <rgordon@lcs.on.ca>
2023-01-30 21:29:23 -0500

    Revised operations on optionals page to make data types clearer.

--- OptionalsAndDataInput.playground/Pages/Operations on Optionals.xcplaygroundpage/Contents.swift
+++ OptionalsAndDataInput.playground/Pages/Operations on Optionals.xcplaygroundpage/Contents.swift
@@ -5,18 +5,33 @@
  
  - Callout(Key Question):
  Can we do arithmetic with optional values?
+ 
+ First, run this page.
+ 
+ Verify that the first four lines of code given work, and that 
   
  Uncomment line 22 below, then try to run this playground page.
  */
 
 // Simulate first input
 let input1 = "20"
-// Simulate second input
-let input2 = "10"
 // Convert first input to integer
 let input1AsInteger = Int(input1)
+// Print data type of first conversion
+print("---")
+print("input1AsInteger has a data type of:")
+print(type(of: input1AsInteger))
+print("===")
+
+// Simulate second input
+let input2 = "10"
 // Convert second input to integer
 let input2AsInteger = Int(input2)
+// Print data type of second conversion
+print("---")
+print("input2AsInteger has a data type of:")
+print(type(of: input2AsInteger))
+print("===")
 
 // Now try to do arithmetic...
 //let area = input1AsInteger * input2AsInteger
