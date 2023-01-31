b6a34ba188e020cff776b883834090b61eafe244
Russell Gordon <rgordon@lcs.on.ca>
2023-01-30 21:02:01 -0500

    Revised introduction page.

--- OptionalsAndDataInput.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ OptionalsAndDataInput.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -2,12 +2,16 @@
  
  # Optionals and Data Input
 
- By now you are familiar with Swift's basic data types:
+ By now you are familiar with these data types:
  
  * String
+    * e.g.: text, such as "Hello world" or "Pineapple on pizza is delicious!"
  * Int
- * Float
+    * e.g.: a number that is not a fraction; a whole number such as: ...-2, -1, 0, 1, 2...
  * Double
+    * e.g.: a number that may be a fractional value; a real number, such as: 3.14159...
+ * Bool
+    * e.g.: a value that is either `true` or `false`
  
  Today, you will examine what happens when users provide input to a program in the form of a string, and how conversions to other data types are handled.
  
