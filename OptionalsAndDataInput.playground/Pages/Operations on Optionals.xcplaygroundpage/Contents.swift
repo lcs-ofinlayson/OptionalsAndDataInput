abd4388915a08134bd9b3d0dedea72e3919d86a4
Russell Gordon <rgordon@lcs.on.ca>
2023-01-30 21:26:40 -0500

    Operations on Optionals page completed.

---
+++ OptionalsAndDataInput.playground/Pages/Operations on Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: [Previous](@previous)
+/*:
+ 
+ ## Operations on optionals
+ 
+ - Callout(Key Question):
+ Can we do arithmetic with optional values?
+  
+ Uncomment line 22 below, then try to run this playground page.
+ */
+
+// Simulate first input
+let input1 = "20"
+// Simulate second input
+let input2 = "10"
+// Convert first input to integer
+let input1AsInteger = Int(input1)
+// Convert second input to integer
+let input2AsInteger = Int(input2)
+
+// Now try to do arithmetic...
+//let area = input1AsInteger * input2AsInteger
+
+/*:
+ 
+ ### Consider
+ 
+ 1. Why does is the Swift compiler programmed in this manner?
+ 
+ Write down your thoughts in your notebook.
+   
+ */
+
+//: [Next](@next)
